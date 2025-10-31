1463b5245382028e91f774902e367db1bb58b7f5
Tushar <tusharanshu18@gmail.com>
2025-10-31 08:50:23 +0530

    intial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {
+BrowserRouter as Router ,
+Routes,
+Route,
+Navigate,
+} from "react-router-dom";
+import {Toaster} from "react-hot-toast"
+const App = () => {
+  return (
+    <div>
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Invoices/InvoiceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InvoiceDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InvoiceDetails
