bf76d7eab35121fe8425d9e6b2b1371ad8d83965
Tushar <tusharanshu18@gmail.com>
2025-11-01 08:00:26 +0530

    added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,33 @@ import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUp from "./pages/Auth/SignUp";
 import Login from "./pages/Auth/Login";
-
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AllInvoices from "./pages/Invoices/AllInvoice";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetails";
+import Profile from "./pages/Profile/Profile";
+import ProtectedRoute from "./pages/Auth/ProtectedRoute";
 const App = () => {
   return (
-    <div>
+    <div> 
       <Router>
         <Routes>
           {/* public routes */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<  SignUp />} />
           <Route path="/login" element={<Login />} />
+
+
+          {/* Protected routes */}
+          <Route path ="/" element={<ProtectedRoute/>}>
+          <Route path = "/dashboard" element ={<Dashboard/>}/>
+          <Route path = "/invoices" element ={<AllInvoices/>}/>
+          <Route path = "/invoices/new" element={<CreateInvoice/>}/>
+          <Route path ="/invoices/:id"   element={<InvoiceDetail/>} />
+          <Route path ="/profile" element ={<Profile/>}/>
+          </Route>
+
+
 
           {/* catch all route  */}
           <Route path="*" element={<Navigate to="/" replace />} />
---
+++ frontend/src/layout/DashboardLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardLayout = ({children}) => {
+  return (
+    <div>
+    dashboard 
+    <div>{children}</div>
+    </div>
+  )
+}
+
+export default DashboardLayout
